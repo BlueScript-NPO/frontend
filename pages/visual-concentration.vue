84b1d3d0eb681c021d16f65cbee7c7f81ed1ebfe
SangAhnAfasys <sangtheahn@gmail.com>
2024-05-27 22:04:55 +0900

    Added boilerplate code to every file
    Added the template and script sections to every file, as it was causing errors

--- pages/visual-concentration.vue
+++ pages/visual-concentration.vue
@@ -1,3 +1,5 @@
+<script setup></script>
+
 <template>This is Visual Concentration</template>
 
 // /VisualConcentration/?time=300&chatLength=5
---
+++ pages/visual-scan.vue
@@ -0,0 +1,17 @@
+// /VisualConcentration/?time=300&chatLength=5
+
+<script setup>
+const route = useRoute();
+
+console.log(route.params.id);
+</script>
+
+<template>
+    <!-- <h1>{{}}</h1>
+    <h1>{{}}</h1>
+    <h1>{{}}</h1>
+    <h1>{{}}</h1>
+    <h1>{{}}</h1> -->
+
+    <h1>test</h1>
+</template>
