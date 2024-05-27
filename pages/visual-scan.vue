c70e914ecd42153703ffa18836121ef5debb2178
SangAhnAfasys <sangtheahn@gmail.com>
2024-05-27 23:29:47 +0900

    Implemented parameter checker
    The code checks the validity of the inputted parameters
    -> If the parameters are valid, use them
    -> If they are not valid/if they don't exist, replace with the default parameters

--- pages/visual-scan.vue
+++ pages/visual-scan.vue
@@ -1,17 +1,70 @@
-// /VisualConcentration/?time=300&chatLength=5
+<!--
+PARAMETERS!!!
+target-type= num / upper / lower / symbol // default = num
+target-count= (Integer) // default = 1
+char-count= (Integer) // default = 10
+target-size= big/small // default = big
+trial-count= (Integer) // default = 3
+ -->
 
 <script setup>
 const route = useRoute();
+const unprocessedParams = route.query;
 
-console.log(route.params.id);
+const paramNames = [
+    "target-type",
+    "target-count",
+    "char-count",
+    "target-size",
+    "trial-count",
+];
+
+const validParams = {
+    "target-type": ["num", "upper", "lower", "symbol"],
+    "target-count": "Number",
+    "char-count": "Number",
+    "target-size": ["big", "small"],
+    "trial-count": "Number",
+};
+
+const defaultParams = {
+    "target-type": "num",
+    "target-count": "1",
+    "char-count": "10",
+    "target-size": "big",
+    "trial-count": "3",
+};
+
+const params = {
+    "target-type": "",
+    "target-count": "",
+    "char-count": "",
+    "target-size": "",
+    "trial-count": "",
+};
+
+function handleParam(paramName) {
+    const paramValue = unprocessedParams[paramName];
+
+    // For number parameters
+    if (validParams[paramName] == "Number") {
+        return Number.isInteger(Number(paramValue))
+            ? paramValue
+            : defaultParams[paramName];
+    }
+
+    // For all other parameters
+    return validParams[paramName].includes(paramValue)
+        ? paramValue
+        : defaultParams[paramName];
+}
+
+paramNames.forEach((paramName) => {
+    params[paramName] = handleParam(paramName);
+});
 </script>
 
 <template>
-    <!-- <h1>{{}}</h1>
-    <h1>{{}}</h1>
-    <h1>{{}}</h1>
-    <h1>{{}}</h1>
-    <h1>{{}}</h1> -->
-
-    <h1>test</h1>
+    <h1>{{ unprocessedParams }}</h1>
+    <h1>{{ params }}</h1>
 </template>
