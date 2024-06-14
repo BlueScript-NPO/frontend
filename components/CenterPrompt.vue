001084586a23845fd8c6adf41617a3299dee4b12
suhjae <jwsuh25@kis.ac>
2024-06-14 15:32:50 +0900

    chore: Seperated parts to components

---
+++ components/CenterPrompt.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  prompt: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="text-main">{{ props.prompt }}</div>
+</template>
---
+++ components/TitleHud.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+  subtitle: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template class="flex flex-col items-center space-y-8">
+  <div class="text-6xl font-light text-center text-white font-mono">
+    {{ props.title }}
+  </div>
+  <div class="text-xl font-light text-center text-white font-mono">
+    {{ props.subtitle }}
+  </div>
+</template>
