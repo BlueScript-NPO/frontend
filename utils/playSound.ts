4a07bfbcc9bad9db6d2cfa6680a3dae7b0f1485d
SuhJae <j@suhjae.dev>
2024-06-21 21:55:05 +0900

    fix: apdated audio method from HTMLAudioElement to  Web Audio API

--- utils/playSound.ts
+++ utils/playSound.ts
@@ -1,6 +1,24 @@
 // utils/playSound.ts
 export function playSound(filename: string, volume = 100) {
-  const audio = new Audio(`/audio/${filename}.wav`);
-  audio.volume = volume / 100;
-  audio.play();
+  const audioContext = new (window.AudioContext ||
+    (window as any).webkitAudioContext)();
+
+  fetch(`/audio/${filename}.wav`)
+    .then((response) => response.arrayBuffer())
+    .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
+    .then((audioBuffer) => {
+      const source = audioContext.createBufferSource();
+      source.buffer = audioBuffer;
+
+      const gainNode = audioContext.createGain();
+      gainNode.gain.value = volume / 100;
+
+      source.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+
+      source.start();
+    })
+    .catch((error) => {
+      console.error("Error playing sound:", error);
+    });
 }
