b42427da9576963aac9a87a87e5521015fdcf244
suhjae <jwsuh25@kis.ac>
2024-07-25 13:52:42 +0900

    chore: prepare for crowdin translation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,17 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   extends: ["@nuxt/ui-pro"],
   modules: ["@nuxtjs/i18n", "@nuxt/ui", "@nuxtjs/i18n"],
+
   ui: {
     icons: ["simple-icons", "heroicons"],
   },
+
   hooks: {
     "prerender:routes"({ routes }) {
       routes.clear(); // Do not generate any routes (except the defaults)
     },
   },
+
   i18n: {
     baseUrl: "https://bluescript.app",
     langDir: "locales",
@@ -31,56 +34,58 @@ export default defineNuxtConfig({
         code: "ko",
         iso: "ko-KR",
         name: "한국어",
-        file: "ko.json",
+        file: "ko/main.json",
       },
       {
         code: "en",
         iso: "en-US",
         name: "English",
-        file: "en.json",
+        file: "en/main.json",
       },
       {
         code: "fr",
         iso: "fr-FR",
         name: "Français",
-        file: "fr.json",
+        file: "fr/main.json",
       },
       {
         code: "es",
         iso: "es-ES",
         name: "Español",
-        file: "es.json",
+        file: "es/main.json",
       },
       {
         code: "ru",
         iso: "ru-RU",
         name: "Русский",
-        file: "ru.json",
+        file: "ru/main.json",
       },
       {
         code: "ja",
         iso: "ja-JP",
         name: "日本語",
-        file: "ja.json",
+        file: "ja/main.json",
       },
       {
         code: "zh",
         iso: "zh-CN",
         name: "中文",
-        file: "zh.json",
+        file: "zh/main.json",
       },
       {
         code: "ar",
         iso: "ar-SA",
         name: "العربية",
-        file: "ar.json",
+        file: "ar/main.json",
       },
       {
         code: "hi",
         iso: "hi-IN",
         name: "हिन्दी",
-        file: "hi.json",
+        file: "hi/main.json",
       },
     ],
   },
+
+  compatibilityDate: "2024-07-25",
 });
