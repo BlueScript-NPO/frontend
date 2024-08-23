83933170454e01b8c824788923928a8ff62d9a0d
suhjae <jwsuh25@kis.ac>
2024-08-23 15:42:58 +0900

    fix: lang switcher

---
+++ components/LangSwitcher.vue
@@ -0,0 +1,56 @@
+<template #right>
+  <USelectMenu
+    :options="langItems"
+    v-model="selectedLang"
+    option-attribute="label"
+    value-attribute="code"
+    variant="none"
+    selected-icon="ph-check"
+  >
+    <template #trailing>
+      <UIcon name="ph-caret-down" class="w-5 h-5" />
+    </template>
+    <template #leading>
+      <span class="mr-2">
+        <img
+          class="h-4 w-auto"
+          :src="'/flags/' + selectedLang + '.svg'"
+          loading="lazy"
+          :alt="selectedLang"
+        />
+      </span>
+    </template>
+    <template #option="{ option }">
+      <img
+        class="h-4 w-auto"
+        :src="'/flags/' + option.code + '.svg'"
+        loading="lazy"
+        :alt="option.code"
+      />
+      <span class="truncate">{{ option.label }}</span>
+    </template>
+  </USelectMenu>
+</template>
+
+<script setup lang="ts">
+const { locale, locales, setLocale } = useI18n();
+
+const selectedLang = ref<string>(locale.value);
+
+// watch lang change
+watch(selectedLang, (val) => {
+  setLocale(val);
+});
+
+const langItems = computed(() => {
+  const items = [];
+
+  for (let i in locales.value) {
+    items.push({
+      label: locales.value[i].name,
+      code: locales.value[i].code,
+    });
+  }
+  return items;
+});
+</script>
