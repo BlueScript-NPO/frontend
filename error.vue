7dc6c3425dc23c9b6f1b397e49255135f14adef0
suhjae <jwsuh25@kis.ac>
2024-09-04 14:16:08 +0900

    feat: error page

---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <div class="w-full h-[80dvh] flex items-center justify-center">
+      <UPageError :error="error" />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const error = useError();
+</script>
